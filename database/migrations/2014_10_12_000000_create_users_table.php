c29b2cd1bc2d0e10078858d8762b83c4d033baaf
Brock Bearchell <brock@spacestud.io>
2017-03-13 19:43:45 -0400

    Added profile image to the users table.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->string('last_name')->nullable();
             $table->string('city')->nullable();
             $table->string('state')->nullable();
+            $table->string('profile_image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
